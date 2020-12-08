cf012e267bb072f803394f943a59d84fc31a6c5a
Björn Ebbinghaus <bjoern@ebbinghaus.me>
2020-12-08 12:20:38 +0100

    Rename function names and add doc

--- 2020/day08.clj
+++ 2020/day08.clj
@@ -20,7 +20,10 @@
           "acc" (recur (inc ip) (+ acc value) (conj already-run ip))
           "jmp" (recur (+ ip value) acc (conj already-run ip)))))))
 
-(defn execute [instructions]
+(defn execute
+  "Executes a vector of instructions.
+  Returns a number, or nil in case of a failure."
+  [instructions]
   (loop [ip 0
          acc 0
          already-run #{}]
@@ -45,18 +48,19 @@
   {"nop" "jmp"
    "jmp" "nop"})
 
-(defn get-variation
+(defn variation
   "Swaps the instruction at position `ip` between 'nop' and 'jmp'."
   [instructions ip]
   (let [[cmd value] (get instructions ip)]
     (assoc instructions ip
       [(replace-cmd cmd cmd) value])))
 
-(defn get-all-possible-variations [instructions]
-  (set (map #(get-variation instructions %) (range (count instructions)))))
+(defn all-variations [instructions]
+  "Return a set of all instructions where at exactly one position a 'nop' replaces a 'jmp' or a 'jmp' replaces a 'nop'."
+  (set (map #(variation instructions %) (range (count instructions)))))
 
 (defn solve-task2 [instructions]
-  (some execute (get-all-possible-variations instructions)))
+  (some execute (all-variations instructions)))
 
 (println "Answer for star 1: " (solve-task1 *input*))
-(println "Answer for star 1: " (time (solve-task2 *input*)))
+(println "Answer for star 2: " (time (solve-task2 *input*)))
